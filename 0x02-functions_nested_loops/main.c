5d5724f38075f1708ae21ec4857262f6a1de16a5
dnMbugua <dennisnjogu333@gmail.com>
2022-07-10 16:56:33 -0700

    Task no. 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,15 @@
-int _putchar(char c);
+#include "main.h"
+/**
+*main - check the code for Holberton School students.
+*Return: Always 0.
+*/
 
-void print_alphabet(void);
+int main(void)
 
-void print_alphabet_x10(void);
+{
+
+	print_alphabet_x10();
+
+	return (0);
+
+}
