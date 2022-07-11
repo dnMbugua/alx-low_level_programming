574025bb4d79542b65e98eba3894b3d58d578377
dnMbugua <dennisnjogu333@gmail.com>
2022-07-11 14:49:49 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a piramid
+ * @size: type int
+ * Return: void
+ */
+
+void print_triangle(int size)
+
+{
+
+	int row, col;
+
+
+
+	if (size > 0)
+
+	{
+
+		for (row = 1; row <= size; row++)
+
+		{
+
+			for (col = size - row; col > 0; col--)
+
+				_putchar(' ');
+
+			for (col = 0; col < row; col++)
+
+				_putchar('#');
+
+
+
+			if (row == size)
+
+				continue;
+
+			_putchar('\n');
+
+		}
+
+	}
+
+	_putchar('\n');
+
+}
